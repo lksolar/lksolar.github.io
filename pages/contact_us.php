4c6a205f4a7236c3620af541f1b95d4d72f1fe52
lksolar <lukekellylukekelly@outlook.com>
2023-01-04 21:42:00 +0000

    test echo

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -1,5 +1,6 @@
 
                 <?php
+                 echo 'Message';
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
