491425e999c4999b3a9d214778592a695c7b0b53
lksolar <lukekellylukekelly@outlook.com>
2023-01-04 18:03:38 +0000

    test email sending php 0401

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -14,11 +14,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-10 col-md-offset-1">
-                                <input id="phone" name="phonenumber" type="text" placeholder="Phone" class="form-control" required>
-                            </div>
-                        </div>
                     <div class="form-group">
                             <div class="col-md-10 col-md-offset-1">
                                 <input id="subject" name="subject" type="text" placeholder="Subject" class="form-control">
@@ -38,47 +33,25 @@
                         </div>
                     </fieldset>
                 </form>
-<?php 
-if(isset($_POST['submit'])) {
-    
-    $to = 'lukekellyphotos5@gmail.com'; 
-    $from = $_POST['email']; 
-    $subject ='Adlivetech Contact Form';
-    $name = $_POST['name']; 
-    $email = $_POST['email']; 
-    $phone = $_POST['phonenumber'];
-    $subject = $_POST['subject']; 
-    $messag = $_POST['messag']; 
-   
-
-$message = '<html><body>';
-
-$message .='<table>';
-$message .='<tr><td>Name:</td><td>'. $name .'</td></tr>';
-
-$message .='<tr><td>Email:</td><td>'. $email .'</td></tr>';
-
-$message .='<tr><td>Phone:</td><td>'. $phone .'</td></tr>';
-
-$message .='<tr><td>Subject:</td><td>'. $subject .'</td></tr>';
-
-$message .='<tr><td>Message:</td><td>'. $messag .'</td></tr>';
-
-$message .='</table>';
-
-$message .='</body></html>';
-
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers .= "To: {$to}\r\n";
-$headers .= "From: {$name} <{$from}>\r\n";
-$headers .= "Reply-To: <{$to}>\r\n";
-$headers .= "Subject: {$subject}\r\n";
-$headers .= "X-Mailer: PHP/".phpversion()."\r\n";
-
-mail($from, $subject, $message, $headers);
+                <?php
+$to_email = "lukeamk@gmail.com";
+$subject = "Test email to send from XAMPP";
+$body = "Hi, This is test mail to check how to send mail from Localhost Using Gmail ";
+$headers = "From: sender email";
+ini_set('SMTP','smtp.gmail.com');
+ini_set('smtp_port',587);
+ini_set('sendmail_from','lukekellysws@gmail.com');
+ini_set('auth_username','lukekellysws@gmail.com');
+ini_set('auth_password','sflhmgesrfpdpaei');
  
+if (mail($to_email, $subject, $body, $headers))
+ 
+{
+    echo "Email successfully sent to $to_email...";
 }
-
-?>
+ 
+else
+ 
+{
+    echo "Email sending failed!";
+}
