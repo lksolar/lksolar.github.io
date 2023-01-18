12e602425d53909795ad9e91ddaf81f001cb8102
lksolar <lukekellylukekelly@outlook.com>
2023-01-18 18:42:14 +0000

    back to actual normal

--- pages/mail_handler.php
+++ pages/mail_handler.php
@@ -10,8 +10,6 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require '../vendor/autoload.php';
-include '../.secrets/secrets.php';
-
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
